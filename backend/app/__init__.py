18141375ff23aca9c3dbe4e10291fb1d209b8130
kevin-l-tran <kltran@csu.fullerton.edu>
2025-07-19 22:10:43 -0700

    feat: establish mongodb connection

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,22 @@
+import os
+
 from flask import Flask
+from flask_mongoengine import MongoEngine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = MongoEngine()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'eijurghredoigjhroduijgjdriog'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'temporary key')
+    
+    app.config['MONGODB_SETTINGS'] = {
+        'db':       os.getenv('MONGO_DB', 'my_database'),
+        'host':     os.getenv('MONGO_HOST', 'localhost'),
+        'port':     int(os.getenv('MONGO_PORT', 27017)),
+    }
 
+    db.init_app(app)
     return app
